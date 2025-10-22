55c3ec3db05f3d4dd9ccebe935e4a5a81fac673e
violetlinedi <wqfwktmqe35873@hotmail.com>
2025-10-22 16:40:18 +0800

    mock zama

---
+++ contracts/mockZama.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.27;
+
+import {ERC7984} from "@openzeppelin/confidential-contracts/token/ERC7984/ERC7984.sol";
+import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
+
+contract MockZama is ERC7984, SepoliaConfig {
+    constructor() ERC7984("mockZama", "mockZama", "") {}
+
+    function mint() external {
+        uint64 amount = 1000*1000000;
+        euint64 encryptedAmount = FHE.asEuint64(amount);
+        _mint(msg.sender, encryptedAmount);
+    }
+}
