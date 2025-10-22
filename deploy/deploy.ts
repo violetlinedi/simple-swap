388802e80a69db022c66d1a6b108bb4f2ac41a3c
violetlinedi <wqfwktmqe35873@hotmail.com>
2025-10-22 16:41:20 +0800

    deploy update

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,26 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const mockEth = await deploy("MockETH", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const mockZama = await deploy("MockZama", {
+    from: deployer,
+    log: true,
+  });
+
+  const simpleSwap = await deploy("SimpleSwap", {
+    from: deployer,
+    log: true,
+    args: [mockEth.address, mockZama.address, 1000],
+  });
+
+  console.log(`MockETH contract: ${mockEth.address}`);
+  console.log(`MockZama contract: ${mockZama.address}`);
+  console.log(`SimpleSwap contract: ${simpleSwap.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_simple_swap"; // id required to prevent reexecution
+func.tags = ["SimpleSwap"];
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,14 +8,17 @@ import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
 import { vars } from "hardhat/config";
 import "solidity-coverage";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 import "./tasks/accounts";
-import "./tasks/FHECounter";
+import "./tasks/simpleSwap";
 
 // Run 'npx hardhat vars setup' to see the list of variables that need to be set
 
 const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const INFURA_API_KEY: string = process.env.INFURA_API_KEY ?? vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
